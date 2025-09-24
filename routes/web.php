57240805dcaafae290eb741dc8f40fc63cdfbe70
Karol Przybysz <k.przybysz@samsung.com>
2025-09-24 15:12:41 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\BookController; 
 
 
@@ -9,3 +10,11 @@ Route::get('/', function () {
 });
 
 Route::get('books', [BookController::class, 'index']);
+
+Route::get('/home', function (){
+    return view('home');
+});
+
+Route::post('/register', [UserController::class, 'register']);
+
+Route::post('/logout', [UserController::class, 'logout']);
