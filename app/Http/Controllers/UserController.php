eea74c3f5ad3430fe57bcb6e031a937163afd22a
Karol Przybysz <k.przybysz@samsung.com>
2025-09-24 15:11:52 +0200

    add register and logout logic

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+        // $incomingFields = $request->validate([
+        //     ''
+       
+    }
+
+    public function register(Request $request){
+        $incomingFields = $request->validate([
+            'name'=>['required','min:3','max:10', Rule::unique('users','name')],
+            'email'=>['required', Rule::unique('users','email')],
+            'password'=>'required',
+        ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/');
+    }
+
+    public function logout(){
+        auth()->logout();
+        return redirect('/home');
+    }
+}
